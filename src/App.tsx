0228f6973bccac451e947043c20a3bacefc71e2c
Naman Tamrakar <namantam1@gmail.com>
2021-12-05 22:18:38 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import "./assets/index.scss";
+import About from "src/components/About";
+
+const App = () => (
+  <>
+  <h1>React Parcel App</h1>
+  <About title="About Component" />
+  </>
+);
+
+export default App;
---
+++ src/components/About.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default function About({ title }: { title: string }) {
+  return <h1>{title}</h1>;
+}
